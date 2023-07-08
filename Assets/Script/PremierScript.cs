c6c6972979f9b12299164c26d82ac1d1407e572a
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-08 18:39:07 +0200

    Moving character with Input

--- Assets/Script/PremierScript.cs
+++ Assets/Script/PremierScript.cs
@@ -18,14 +18,36 @@ public class PremierScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0,0,1);
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0,0,-1);
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(0,1,0);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-1,0,0);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(1,0,0);
+        }
     }
 
     private void OnMouseUp() 
     {
         // Ici, Random.Range génère un nombre aléatoire entre 0 et 1
-        float val = Random.Range(0f, 1f);
-        rend.material.color = new Color(val,val,val);
+        rend.material.color = new Color(Random.Range(0f, 1f),Random.Range(0f, 1f),Random.Range(0f, 1f));
         rb.useGravity = true; 
     }
 
