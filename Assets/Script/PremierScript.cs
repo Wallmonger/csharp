ebbfe5208f5c2d3ad654a8d73b94efd2f5ce49ae
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-08 18:24:51 +0200

    Onmouseup and random numbers

--- Assets/Script/PremierScript.cs
+++ Assets/Script/PremierScript.cs
@@ -4,35 +4,31 @@ using UnityEngine;
 
 public class PremierScript : MonoBehaviour
 {
-
-    public string couleur = "rouge";
-    public int nbVie = 3;
-    public float or = 3.5f;
-    public float subOr = 1.1f; 
-    public bool estActif = false;
-    public Vector3 speed;
-
-    // Special var for colors in unity
-    public Color col;
-    // We also can include components directly.
     public Rigidbody rb;
     public Renderer rend;
-
-
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rend.material.color = col;
-        rb.useGravity = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(speed * Time.deltaTime);
-        rend.material.color = col;
+        
     }
+
+    private void OnMouseUp() 
+    {
+        // Ici, Random.Range génère un nombre aléatoire entre 0 et 1
+        float val = Random.Range(0f, 1f);
+        rend.material.color = new Color(val,val,val);
+        rb.useGravity = true; 
+    }
+
+    
+
 }
